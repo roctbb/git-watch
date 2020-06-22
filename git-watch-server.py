d906d7447dc63e2449f9e0e69f76c603734294b2
roctbb <roctbb@gmail.com>
2020-06-22 21:40:42 +0300

    dev

--- git-watch-server.py
+++ git-watch-server.py
@@ -1,13 +1,20 @@
 from flask import Flask, request
 from config import *
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def process():
-    print(request.headers)
-    print(request.data)
-    return 'Hello, World!'
+    event = request.json
+    try:
+        if event['object_kind'] == 'push':
+            repository = event['repository']['name']
+            os.system('./scripts/{}'.format(SCRIPTS[repository]))
+    except Exception as e:
+       print(e)
+
+    return "OK"
 
 app.run(host=HOST, port=PORT)
