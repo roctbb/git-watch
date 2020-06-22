533abd19360326b75c7f593c0ef0a55cdfe84bb7
roctbb <roctbb@gmail.com>
2020-06-22 20:57:10 +0300

    Initial commit

---
+++ git-watch-server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from config import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def process():
+    print(request.headers)
+    print(request.data)
+    return 'Hello, World!'
+
+app.run(host=HOST, port=PORT)
